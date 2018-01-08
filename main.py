22f85e17989ec83a3ff8afb45ae4150a734589b7
Adelia Joseph <asjoseph@mail.usf.edu>
2018-01-07 20:16:30 -0500

    User Sign Up page created as well as two hyperlin templates

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template ('login.html')
+    else: 
+        return "Hello World!"
+
+@app.route('/signed_up/', methods=['POST'])
+def signed_up():
+    return render_template('login1.html')
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12) 
+    app.run(debug=True, host='0.0.0.0', port=4000)
